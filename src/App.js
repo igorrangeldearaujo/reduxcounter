c406d293515c71bfbd2f33c7bcfef11c7e0a4175
Igor <Igor44956@gmail.com>
2020-06-26 17:14:28 -0300

    initial version

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Counter from './Counter'
+import DisplayCounter from './DisplayCounter'
+import { createStore }  from 'redux'
+import { Provider } from 'react-redux'
+
+import counterReducer from './reducer'
+ 
+let store = createStore(counterReducer)
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Counter />
+        <DisplayCounter />
+      </div>
+    </Provider>
+  )
+}
+
+export default App
---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { increment, decrement } from './actions'
+
+const Counter = ({count, increment, decrement}) => {
+   return(
+       <p>
+        Contador: {count}
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+       </p>
+   ) 
+
+}
+
+const mapStateToProps = (state) => {
+    return {
+        count: state.count    
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increment: () => dispatch(increment),
+        decrement: () => dispatch(decrement)
+    }
+}
+
+export default connect(
+                mapStateToProps, 
+                mapDispatchToProps)
+            (Counter)
